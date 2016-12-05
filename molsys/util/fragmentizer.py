c9c069f0104ee9d54a20eec650b1b086a72bf3af
Julian Keupp <julian.keupp@rub.de>
2016-12-05 12:33:13 +0100

    change fragmentizer to check big fragments first and add get_fragtypes_list for unique fragtypes incl count

--- molsys/util/fragmentizer.py
+++ molsys/util/fragmentizer.py
@@ -11,6 +11,7 @@ Created on Mon Nov 28 18:29:19 2016
 """
 
 import os
+import numpy
 import logging
 import glob
 import molsys
@@ -31,19 +32,29 @@ class fragmentizer:
             self.frag_path = os.environ["MOLSYS_FRAGS"]
         else:
             self.frag_path = "."
+        self.read_frags()
+        return
+        
+    def read_frags(self):
         logger.info("Fragmentizer reading frags from %s" % self.frag_path)
         frag_files = glob.glob(self.frag_path + "/*.mfpb")
         self.fragments = {}
+        frag_natoms,fragnames=[],[]
         for f in frag_files:
+            print f
             m = molsys.mol()
             m.read(f, ftype="mfpx")
+            frag_natoms.append(m.natoms)
             m.addon("graph")
             m.graph.make_graph()
             # m.graph.plot_graph(f)
             logger.info("read %s" % f)
             fragname = f.split("/")[-1].split(".")[0]
+            fragnames.append(fragname)
             self.fragments[fragname] = m
-        return
+        ### okay, we want the large fragments to be tested first
+        self.frag_order = [fragnames[i] for i in numpy.argsort(frag_natoms)[::-1]]#[::-1]]
+        print self.frag_order 
 
     def __call__(self, mol):
         """
@@ -56,15 +67,20 @@ class fragmentizer:
         """
         mol.addon("graph")
         mol.graph.make_graph()
+        mol.set_nofrags()
         # mol.graph.plot_graph("mol")
         fi = 0
-        for f in self.fragments.keys():
+        for f in self.frag_order:
             fidx = mol.graph.find_fragment(self.fragments[f])
             for flist in fidx:
+                exists=False
                 for i in flist:
+                    if mol.fragtypes[i] != '0':
+                        exists=True
+                        break
                     mol.fragtypes[i]   = f
                     mol.fragnumbers[i] = fi
-                fi += 1
+                if not exists: fi += 1
         # print mol.fragtypes
         # print mol.fragnumbers
         return
