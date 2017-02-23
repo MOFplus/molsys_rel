a27fc349d982b0ebe4a11e182e6572edb32e6022
Johannes Duerholt <johannes.duerholt@rub.de>
2017-02-23 10:32:13 +0100

    added transformer module to util in oder to transform mol object to other objects like pymatgen or ase, currently only to_pymatgen is possible

---
+++ molsys/util/transformer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import logging
+
+logger = logging.getLogger("molsys.transformer")
+
+def to_pymatgen(mol):
+    try:
+        import pymatgen as mg
+    except ImportError:
+        logger.error("Pymatgen not available!")
+        exit()
+    latt = mg.Lattice.from_parameters(*mol.get_cellparams())
+    elems = mol.get_elems()
+    nelems = []
+    for e in elems: nelems.append(e.title())
+    structure = mg.Structure(latt, nelems, mol.get_frac_xyz())
+    return structure
+
