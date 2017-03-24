2ebc4e0d8af688c9da477aadc4468bbd612145cb
Rochus Schmid <rochus.schmid@rub.de>
2017-03-24 08:21:58 +0100

    added logger writeout of timier

--- molsys/util/timing.py
+++ molsys/util/timing.py
@@ -3,12 +3,16 @@ from __future__ import print_function
 # Copyright (C) 2003  CAMP
 # Please see the accompanying LICENSE file for further information.
 
+# RS: added write_logger
+
 
 import sys
 import time
 import functools
 
-    
+from cStringIO import StringIO
+
+
 def function_timer(func, *args, **kwargs):
     out = kwargs.pop('timeout', sys.stdout)
     t1 = time.time()
@@ -17,34 +21,34 @@ def function_timer(func, *args, **kwargs):
     print(t2 - t1, file=out)
     return r
 
-    
+
 class Timer:
     """Timer object.
-    
+
     Use like this::
-    
+
         timer = Timer()
         timer.start('description')
         # do something
         timer.stop()
-        
+
     or::
-        
+
         with timer('description'):
             # do something
-            
+
     To get a summary call::
-        
+
         timer.write()
 
     """
-    
+
     def __init__(self, print_levels=1000):
         self.timers = {}
         self.t0 = time.time()
         self.running = []
         self.print_levels = print_levels
-    
+
     def print_info(self, calc):
         """Override to get to write info during calculator's initialize()."""
         pass
@@ -53,7 +57,7 @@ class Timer:
         names = tuple(self.running + [name])
         self.timers[names] = self.timers.get(names, 0.0) - time.time()
         self.running.append(name)
-        
+
     def stop(self, name=None):
         if name is None:
             name = self.running[-1]
@@ -68,12 +72,12 @@ class Timer:
 
     def __call__(self, name):
         """Context manager for timing a block of code.
-        
+
         Example (t is a timer object)::
 
             with t('Add two numbers'):
                 x = 2 + 2
-                
+
             # same as this:
             t.start(Add two numbers')
             x = 2 + 2
@@ -81,16 +85,31 @@ class Timer:
         """
         self.start(name)
         return self
-        
+
     def __enter__(self):
         pass
-        
+
     def __exit__(self, *args):
         self.stop()
-            
+
     def get_time(self, *names):
         return self.timers[names]
-                
+
+    def write_logger(self, log_level_func):
+        """
+        RS write using write to a string intead of a file and pass this to the logger (a bit of a hack :-)
+
+        :Parameter:
+
+            - log_level_func : a function of the logger (like logger.info)
+        """
+        timer_report = StringIO()
+        timer_report.write("Timer report:\n")
+        self.write(out=timer_report)
+        log_level_func(timer_report.getvalue())
+        timer_report.close()
+        return
+
     def write(self, out=sys.stdout):
         were_running = list(self.running)
         while self.running:
@@ -107,7 +126,7 @@ class Timer:
         out.write('%-*s    incl.     excl.\n' % (n, 'Timing:'))
         out.write(line)
         tother = tot
-        
+
         inclusive = self.timers.copy()
         exclusive = self.timers.copy()
         keys = sorted(exclusive.keys())
@@ -141,7 +160,7 @@ class Timer:
         out.write('%-*s%9.3f %5.1f%%\n' % (n + 10, 'Total:', tot, 100.0))
         out.write(line)
         out.write('date: %s\n' % time.asctime())
-        
+
         for name in were_running:
             self.start(name)
 
@@ -149,10 +168,10 @@ class Timer:
         for name, t in timer.timers.items():
             self.timers[name] = self.timers.get(name, 0.0) + t
 
-            
+
 class timer:
     """Decorator for timing a method call.
-    
+
     Example::
 
         from ase.utils.timing import timer, Timer
@@ -160,15 +179,15 @@ class timer:
         class A:
             def __init__(self):
                 self.timer = Timer()
-                
+
             @timer('Add two numbers')
             def add(self, x, y):
                 return x + y
-                
+
         """
     def __init__(self, name):
         self.name = name
-    
+
     def __call__(self, method):
         @functools.wraps(method)
         def new_method(slf, *args, **kwargs):
