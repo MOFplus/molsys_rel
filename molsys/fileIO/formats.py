301fe1ebe992722ef5fbdcfafe2c525955ac9b26
Johannes Duerholt <johannes.duerholt@rub.de>
2017-06-09 22:31:19 +0200

    implemented turbo coord format for fileIO

--- molsys/fileIO/formats.py
+++ molsys/fileIO/formats.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import xyz, txyz, mfpx, cif
+import xyz, txyz, mfpx, cif, turbo
 
 read = {
         'xyz':xyz.read,
         'txyz':txyz.read,
         'mfpx':mfpx.read,
-        'cif':cif.read}
+        'cif':cif.read,
+        'turbo':turbo.read}
 
 write = {
         'xyz':xyz.write,
         'txyz':txyz.write,
         'mfpx':mfpx.write,
-        'cif':cif.write} 
+        'cif':cif.write,
+        'turbo': turbo.write} 
 
 #def read(mol, filename, fmt, **kwargs):
     #reads[fmt](mol,filename, **kwargs)
---
+++ molsys/fileIO/turbo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  9 18:17:13 2017
+
+@author: johannes
+"""
+
+import numpy
+import string
+from molsys.util.units import angstrom
+
+def read(mol, f):
+    coord = False
+    xyz = []
+    elems = []
+    for line in f:
+        sline = string.split(line)
+        if sline[0][0] == "$":
+            if sline[0] == "$coord": 
+                coord = True
+                continue
+            else:
+                coord = False
+                continue
+        if coord:
+            xyz.append(map(float,sline[:3]))
+            elems.append(sline[3])
+    f.close()
+    mol.natoms = len(elems)
+    mol.xyz = numpy.array(xyz)*angstrom
+    mol.elems = elems
+    mol.atypes = elems
+    mol.set_empty_conn()
+    mol.set_nofrags()
+    return
+
+
+def write(mol, fname):
+    f = open(fname, "w")
+    f.write("$coord\n")
+    c = mol.xyz/angstrom
+    for i in xrange(mol.natoms):
+        f.write("  %19.14f %19.14f %19.14f   %-2s\n" % 
+                (c[i,0],c[i,1], c[i,2], mol.elems[i]))
+    f.write("$end\n")
+    f.close()
+    return
