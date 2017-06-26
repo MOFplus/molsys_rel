7ec9a1ec0f8c510f48cb6f7ff27493fe9cd16bef
Johannes Duerholt <johannes.duerholt@rub.de>
2017-06-26 16:25:19 +0200

    fixed bug concerning writeout of charges

--- molsys/util/ff2pydlpoly.py
+++ molsys/util/ff2pydlpoly.py
@@ -158,9 +158,9 @@ class wrapper(object):
             if potential != "gaussian": 
                 raise ValueError("Chargetype %s not implemented" % potential)
             chargesum += params[0]
-            f.write("   %8s  %10.4f %10.4f %12.6f 1 %1d\n" % 
+            f.write("   %8s  %10.4f %12.8f %12.8f 1 %1d\n" % 
                     (atype, self.get_mass()[i], params[0], params[1], 0))
-        print "Net charge of the system: %10.5f" % chargesum
+        print "Net charge of the system: %12.8f" % chargesum
         ### bonded potentials ###
         for ict in ["bnd", "ang", "dih", "oop"]:
             buffer_out = ""
