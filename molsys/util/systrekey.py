839392b9dfba74d0f59e70f221a4245f23dfa502
Rochus Schmid <rochus.schmid@rub.de>
2019-09-29 20:56:44 +0200

    added systre arc file reader and DB

--- molsys/util/systrekey.py
+++ molsys/util/systrekey.py
@@ -1,4 +1,11 @@
 """a simple wrapper to run the javascript systrekey from python
+
+    TBI: i guess it would be much better to use json to communicate with javascript
+         -> need to figure out how to do this.
+
+    added a class for systrekey handling
+
+    author: R. Schmid
 """
 
 import os
@@ -10,6 +17,78 @@ import molsys
 # get my path 
 molsys_path = os.path.dirname(molsys.__file__)
 systre_path = os.path.dirname(os.path.dirname(molsys_path)) + "/systreKey"
+
+# path to the curent arc file -- should be called RCSR.arc by default (softlink to current version)
+arc_file = os.path.dirname(__file__)+"/RCSR.arc"
+
+# we read the arc file once when the first instance is made 
+arc_read = False
+
+db_key2name = {}
+db_name2key = {}
+
+# shift_map  = {"-1" : "-", "0": "0", "1": "+"}
+# shift_rmap = {"-" : "-1", "0": "0", "+": "1"}
+
+class systre_db:
+
+    def __init__(self):
+        if not arc_read:
+            self.read_arc()
+        return
+
+    def read_arc(self):
+        global db_key2name, db_name2key, arc_read
+        f = open(arc_file, 'r')
+        for line in f:
+            sline = line.split()
+            if len(sline)>0:
+                if sline[0] == 'key':
+                    key = sline[1:]
+                elif sline[0] == 'id':
+                    name = sline[1]
+                elif sline[0] == "end":
+                    # end of record .. store in directories only 3dim nets
+                    if key[0] == "3":
+                        key = string.join(key)
+                        db_key2name[key] = name
+                        db_name2key[name] = key
+                else:
+                    pass
+        f.close()
+        arc_read=True
+        return
+
+    def get_key(self, name):
+        return db_name2key[name]
+
+    def get_name(self, key):
+        return db_key2name[key]
+
+    # def compress_key(self, skey):
+    #     """compress a systrekey
+        
+    #     Args:           
+    #         self (string or list): original systre key
+    #     """
+    #     if type(skey) == type(""):
+    #         lsk = skey.split()
+    #     else:
+    #         lsk = skey
+    #     lsk.reverse()
+    #     dim = lsk.pop() # dimension
+    #     assert dim == "3"
+    #     csk = ""
+    #     while len(lsk)>0:
+    #         i = lsk.pop()
+    #         j = lsk.pop()
+    #         csk += "%s:%s:" % (i,j)
+    #         for i in range(3):
+    #             csk += shift_map[lsk.pop()]
+    #         if len(lsk)>0:
+    #             csk+= ":"
+    #     return csk
+
 
 def run_systrekey(edges, labels):
     """run javascript systrekey
@@ -24,7 +103,7 @@ def run_systrekey(edges, labels):
         lqg_string += "%d %d" % tuple(e)
         lqg_string += " %d %d %d|" % tuple(l)
     lqg_string = lqg_string[:-1]
-    print lqg_string # DEBUG
+    # print lqg_string # DEBUG
 
     key = subprocess.check_output(args=["node", molsys_path+"/util/run_systrekey.js", lqg_string, systre_path])
     key = key[:-1] # remove newline
