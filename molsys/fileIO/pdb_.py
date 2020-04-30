36f88cbee640551497ecd6ec5ec8c3179ee38a4d
Julian <julian.keupp@rub.de>
2020-04-30 18:19:06 +0200

    fix pdb writer for >99999 atoms

--- molsys/fileIO/pdb_.py
+++ molsys/fileIO/pdb_.py
@@ -16,8 +16,12 @@ def write(mol, f, resnames = None, residx = None, end = True):
     format("CRYST1",mol.cellparams[0],mol.cellparams[1],mol.cellparams[2],mol.cellparams[3],mol.cellparams[4],mol.cellparams[5]))
     # write coordinates
     for i in range(mol.natoms):
+        if i >= 99998: 
+            ii = 99998
+        else:
+            ii = i
         f.write("{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}\n".
-        format("ATOM",i+1,mol.elems[i].upper(),"", resnames[i], "X", residx[i],"",mol.xyz[i,0],mol.xyz[i,1],mol.xyz[i,2],1,0,mol.elems[i],""))
+        format("ATOM",ii+1,mol.elems[i].upper(),"", resnames[i], "X", residx[i],"",mol.xyz[i,0],mol.xyz[i,1],mol.xyz[i,2],1,0,mol.elems[i],""))
     if end:
         f.write("END\n")
     else:
