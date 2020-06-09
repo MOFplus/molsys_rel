3ee9604fbc707266d2bc950f583bfe6e61b37eeb
Özlem Yönder <oezlem.yoender@ruhr-uni-bochum.de>
2020-06-09 15:02:23 +0200

    No major changes

--- molsys/util/turbomole.py
+++ molsys/util/turbomole.py
@@ -1,9 +1,11 @@
 """
 This module contains Turbomole related tools.
 """
+import sys
 import os
 import numpy
 import tempfile
+import time
 import shutil
 import molsys
 from   molsys.util.units import angstrom
@@ -39,20 +41,20 @@ class GeneralTools:
             self.path = path
         return
         
-    def invoke_define(self, define_in_name='define.in', coord_name='coord'):
+    def invoke_define(self, define_in_name='define.in', define_out_name = 'define.out', coord_name='coord'):
         coord_path = os.path.join(self.path,coord_name)
         define_in_path = os.path.join(self.path,define_in_name)
         if not os.path.isfile(coord_path):
             raise FileNotFoundError("Please provide a coord file for invoking define.")
         if not os.path.isfile(define_in_path):
             raise FileNotFoundError("Please provide an input file for invoking define.")
-        out_path = os.path.join(self.path, "define.out")
+        out_path = os.path.join(self.path, define_out_name)
         err_path = os.path.join(self.path, "errormessage")
         os.system('define < %s > %s 2> %s' %(define_in_path, out_path, err_path))
         with open(err_path) as err:
             for line in err:
                 if "define ended normally" not in line:
-                    raise DefineEndedAbnormallyError(f"Define ended abnormally. Check the define input %s." % f_name)
+                    raise DefineEndedAbnormallyError("Define ended abnormally. Check the define input under %s." % define_in_path)
                 else:
                     os.remove(err_path)
         return
@@ -80,20 +82,22 @@ class GeneralTools:
         return mol
 
     def read_charge_from_control(self):
+        c = 0
         control_path = os.path.join(self.path,"control")
         if not os.path.isfile(control_path):
             raise FileNotFoundError("There is no control file in the directory %s." % self.path)
         with open(control_path) as control:
             for line in control:
                 if line.startswith("$charge"):
-                    charge = next(control).split()[0]
-        return charge
+                    charge = float(next(control).split()[0])
+        c = round(charge)
+        return c
 
     def get_nalpha_and_nbeta_from_ridft_output(self, ridft_out_name='ridft.out'):
         """ Read the number of alpha and beta electrons from the ridft output file. """
         ridft_path = os.path.join(self.path, ridft_out_name)
         if not os.path.isfile(ridft_path):
-            raise FileNotFoundError("There is no ridft output file named %s in the directory %s." %(ridft_out_name, path))
+            raise FileNotFoundError("There is no ridft output file named %s in the directory %s." %(ridft_out_name, self.path))
         nalpha = 0
         nbeta = 0
         with open(ridft_path) as ridft_out:
@@ -166,7 +170,7 @@ class GeneralTools:
         f.write("%title\n")
         f.write("%s\n" % title)
         f.write("%method\n")
-        f.write("ENRGY :: ri-u%s [gen_mult = %d, scf_dsta = 1, gen_symm = c1, scf_msil = 1000, scf_rico = %d, for_maxc = %d]\n" %
+        f.write("ENRGY :: ri-u%s [gen_mult = %d, gen_symm = c1, scf_dsta = 1, scf_msil = 1000, scf_rico = %d, for_maxc = %d]\n" %
                 (lot_DFT, M_start, max_mem, max_mem))
         f.write("%coord\n")
         for i in range(len(elems)):
@@ -183,7 +187,7 @@ class GeneralTools:
     def run_tmole(self):
         maindir = os.getcwd()
         os.chdir(self.path)
-        os.system("tmole")
+        os.system("tmole &>/dev/null")
         self.check_dscf_converged()
         os.chdir(maindir)
         return
@@ -249,16 +253,26 @@ class GeometryTools:
         invokes define in a temporary folder to get the assigned symmetry.
         """
         ### create a tmp directory ### 
-        tmp_path = tempfile.mkdtemp()
+        curdir = os.getcwd()
+        tmp_path = os.path.join(curdir,"tmp")
+        os.mkdir(tmp_path)
+        #tempfile.mkdtemp()
         GeneralTools(tmp_path).write_coord_from_mol(mol)
         ### write a define file ###
         GeometryTools._write_define_check_symmetry(tmp_path)
         ### invoke define ###
-        GeneralTools(tmp_path).invoke_define()
+        os.chdir(tmp_path)
+        os.system("define < define.in > define.out")
+        os.chdir(curdir)
         ### read the assigned point group ###
-        point_group = GeometryTools.get_point_group_from_control()
+        point_group = GeometryTools.get_point_group_from_control(tmp_path)
         ### remove the tmp directory ###
         shutil.rmtree(tmp_path)
+        return point_group
+
+    def get_point_group_from_coord(path=os.getcwd(), coord_name='coord'):
+        mol = GeneralTools(path).coord_to_mol(coord_name)
+        point_group = GeometryTools.get_point_group_from_mol(mol)
         return point_group
 
     def _write_define_check_symmetry(path=os.getcwd()):
@@ -278,38 +292,6 @@ class GeometryTools:
         return point_group
 
 
-    def change_to_abelian_symmetry_group(path=os.getcwd()):
-        """
-        Changes the symmetry group to an abelian subgroup. 
-        nel = number of singly occupied electrons
-        """
-        abelian_point_groups = { 'oh':'d2h',    'td':'c2v',    'th':'s6',      't':'d2',
-                                'd3d':'s6',    'd2d':'c2v',   'd4h':'d2h',   'c4v':'c2v',
-                                'c3v':'cs',    'c6v':'c2v',    'd4':'d2',     'd3':'c3'}
-        point_group = GeometryTools.get_point_group_from_control(path)
-        if point_group not in ['c1','cs']:
-            ### determine the point group to be assigned ###
-            try:
-                new_point_group = abelian_point_groups[point_group]
-            except:
-                new_point_group = 'c1'
-                print("The point group %s is not in the dictionary pointgroupdict." % point_group)
-            print("It will try to assign the subgroup %s." % new_point_group)
-            natoms = GeometryTools._get_natoms_from_control(path)
-            ### write the define.in file ###
-            GeometryTools._write_define_new_point_group(new_point_group, path)
-            ### invoke define ###
-            GeneralTools(path).invoke_define()
-            ### check if the number of atoms is still the same ###
-            newnatoms = GeometryTools._get_natoms_from_control(path)
-            if natoms != newnatoms:
-                raise SymmetryAssignmentChangedtheStructureError("The structure is far from the symmetry group %s. Therefore, while trying to change the symmetry group, new atoms are added to enforce it." % new_point_group)
-                print('The number of atoms is not the same!')
-        else: 
-            print('The detected point group is already c1 or cs!')
-        return
-
-
     def _write_define_new_point_group(new_point_group='c1', path=os.getcwd()):
         ### get the number of alpha and beta electrons ###
         nalpha, nbeta = GeneralTools(path).get_nalpha_and_nbeta_from_ridft_output()
@@ -327,7 +309,7 @@ class GeometryTools:
         f.write("\n")
         f.write("eht\n")
         f.write("\n")
-        f.write("%s\n" % charge)
+        f.write("%d\n" % charge)
         f.write("n\n")
         f.write("u %d\n" % abs(nalpha-nbeta))
         f.write("*\n\n\nq")
@@ -343,52 +325,29 @@ class GeometryTools:
                     natoms = int(lines.strip().split('=')[-1])
         return natoms
 
-    def assign_symmetry(path=os.getcwd()):
-        changed = False
-        ### get the number of alpha and beta electrons ###
-        nalpha, nbeta = GeneralTools(path).get_nalpha_and_nbeta_from_ridft_output()
-        ### get the charge ###
-        charge = GeneralTools(path).read_charge_from_control()
-        ### change the directory ###
-        curdir = os.getcwd()
-        os.chdir(path)
-        os.system("cpc symmetry")
-        tmp_path = os.path.join(path,"symmetry")
-        define_in_file = os.path.join(tmp_path, "define.in")
-        f = open(define_in_file,"w")
-        f.write("\n")
-        f.write("y\n")
-        f.write("desy 1d-5\n")
-        f.write("ired\n")
-        f.write("*\n")
-        f.write("\n")
-        f.write("eht\n")
-        f.write("\n")
-        f.write("%s\n" % charge)
-        f.write("n\n")
-        f.write("u %d\n" % abs(nalpha-nbeta)) 
-        f.write("*\n\n\nq")
-        f.close()
-        tmp_path = os.path.join(path,"symmetry")
-        try:
-            GeneralTools(tmp_path).invoke_define()
-        except:
-            GeometryTools.change_to_abelian_symmetry_group(tmp_path)
-        point_group = GeometryTools.get_point_group_from_control(tmp_path)
-        if point_group != "c1":
-            changed = True
-            for f in os.listdir(path):
-                if os.isfile(f):
-                    os.remove(f)
-            for f in os.listdir(tmp_path):
-                shutil.move(f,path)
-        shutil.rmtree(tmp_path)
-        return changed, point_group
+    def change_point_group(path=os.getcwd(), point_group='c1'):
+        non_abelian_point_groups = {  'o':'d2',  'oh':'d2h',  'td':'c2v',  'th':'s6',    't':'d2',
+                                'd2d':'c2v','d3d':'s6',  'd4d':'s8',  'd5d':'s10', 'd6d':'s12','d7d':'s14','d8d':'s16',
+                                'd3h':'c2v','d4h':'c4h', 'd5h':'c2v', 'd6h':'c2h', 'd7h':'c2v', 'd8h':'c2h',
+                                'c3v':'cs', 'c4v':'c2v', 'c5v':'c5',  'c6v':'c2v', 'c7v':'c7', 'c8v':'c8',
+                                 'd3':'c3',  'd4':'c4',   'd5':'c5',   'd6':'c6',   'd7':'c7',  'd8':'c8'}
+        if point_group in non_abelian_point_groups:
+            point_group_to_assign = non_abelian_point_groups[point_group] 
+        else:
+            point_group_to_assign = point_group
+        ### Get the number of atoms ###
+        natoms = GeometryTools._get_natoms_from_control(path)
+        ### write the define.in file ###
+        GeometryTools._write_define_new_point_group(point_group_to_assign, path)
+        ### invoke define ###
+        GeneralTools(path).invoke_define(define_out_name = "define-test.out")
+        ### check if the number of atoms is still the same ###
+        newnatoms = GeometryTools._get_natoms_from_control(path)
+        if natoms != newnatoms:
+            raise SymmetryAssignmentChangedtheStructureError("The structure is does not follow the  point group %s symmetry operations. Therefore, while trying to change the symmetry group, new atoms are added to enforce it." % new_point_group)
+        return point_group_to_assign
 
         
-
-
-
 
 class Mol:
 
@@ -442,7 +401,7 @@ class Mol:
         return self.MM
 
 
-class Submit:
+class SubmissionTools:
     """Performs submission of the jobs."""
     def __init__(self, path=os.getcwd()):
         if not os.path.isdir(path):
@@ -497,6 +456,8 @@ class Submit:
         if os.path.isfile(f_path):
                 converged = True
                 os.remove(f_path)
+        else:
+            sys.exit("Geometry optimization did not converge.")
         return converged
 
     def check_imaginary_frequency(self):
@@ -535,6 +496,36 @@ class Submit:
         os.chdir(maindir)
         return
 
+    def common_workflow(self, active_atoms, max_mem, md_xyz_path, lot_DFT):
+        mol = molsys.mol.from_file(md_xyz_path, "xyz")
+        """determines the orbital occupations before through Fermi smearing."""
+        nel = Mol(mol).count_number_of_electrons()
+        if (nel % 2) == 0:
+            M_start = 3
+        else:
+            M_start = 2
+        new_xyz = GeometryTools.add_noise(mol, active_atoms)
+        GT = GeneralTools(self.path)
+        # Perform a single point calculation using Fermi smearing
+        GT.make_tmole_dft_input_for_fermi_smearing(
+                mol.elems, 
+                new_xyz, 
+                M_start, 
+                max_mem, 
+                md_xyz_path, 
+                lot_DFT)
+        GT.run_tmole()
+        # Remove the data group $fermi from the control file
+        GT.kdg("fermi")
+        # If there are partial occupations round them to integers
+    #    GT.round_fractional_occupation()
+        atom = False
+        GT.ridft()
+        if mol.natoms == 1:
+            atom = True
+            GT.check_dscf_converged()
+            os.system("touch found")
+        return atom 
 
     def transition_state_workflow(self, active_atoms):
         found = False
@@ -542,27 +533,135 @@ class Submit:
         self.jobex()
         if not self._check_geo_opt_converged():
             print("Transition state pre-optimization did not converge.")
-        return 
-
+        return found
 
     def minima_workflow(self):
         found = False
+        point_group_initial = GeometryTools.get_point_group_from_coord(self.path,'coord')
+        print("point_group_initial", point_group_initial)
         self.jobex()
-        if not self._check_geo_opt_converged():
-            print("Geometry optimization did not converge.")
-        else:
-            changed, point_group = GeometryTools.assign_symmetry(self.path)
-            if changed:
-                print("The point group is changed to %s." % point_group)
-                self.jobex()
-            self.aoforce()
-            inum, imfreq = self.check_imaginary_frequency()
-            if inum == 0:
-                found = True
-                print("The minima found succesfully.")
+        self._check_geo_opt_converged()
+        point_group_final = GeometryTools.get_point_group_from_coord(self.path,'coord')
+        print("point_group_final", point_group_final)
+        if point_group_final != "c1":
+            point_group_assigned = GeometryTools.change_point_group(self.path, point_group_final)
+            if point_group_assigned == point_group_final:
+                print("The point group is changed to %s." % point_group_final)
             else:
-                print("There are imaginary frequencies. This is not a minima.")
+                print("The molecule has point group of %s. However, the abelian point group %s is assigned." % ( point_group_final, point_group_assigned))
+            GeneralTools(self.path).ridft()
+            self.jobex()
+            self._check_geo_opt_converged()
+        self.aoforce()
+        inum, imfreq = self.check_imaginary_frequency()
+        if inum == 0:
+            found = True
+            print("The minima found succesfully.")
+        else:
+            print("There are imaginary frequencies. This is not a minima.")
         return found
+
+    def submit(self, foffset, active_atoms):
+        if foffset == 0:
+            found = self.transition_state_workflow(active_atoms)
+        else:
+            found = self.minima_workflow()
+        return found
+
+    def write_submit_py(self, foffset, active_atoms, max_mem, md_xyz_path, lot_DFT):
+        active_atoms_string = ','.join([str(atom) for atom in active_atoms])
+        f_path = os.path.join(self.path,"submit.py")
+        f = open(f_path,"a")
+        f.write("import os\n")
+        f.write("import molsys\n")
+        f.write("from molsys.util import turbomole\n")
+        f.write("# Submission Tools\n")
+        f.write("ST = turbomole.SubmissionTools()\n")
+        f.write("atom = ST.common_workflow(active_atoms = [%s],\n"% active_atoms_string )
+        f.write("                   max_mem = %d,\n" % max_mem)
+        f.write("                   md_xyz_path = '%s',\n" % md_xyz_path)
+        f.write("                   lot_DFT = '%s')\n" % lot_DFT)
+        f.write("if not atom:\n")
+        if foffset == 0:
+            f.write("    found = ST.transition_state_workflow([%s])\n" % active_atoms_string)
+        else:
+            f.write("    found = ST.minima_workflow()\n")
+        f.write("    if found:\n")
+        f.write("        os.system('touch found')\n")
+        f.write("    else:\n")
+        f.write("        os.system('touch notfound')")
+        f.close()
+        return
+
+
+class Slurm:
+
+#    def get_avail_nodes():
+#        sinfo = os.popen('sinfo --format="%n %t %c %m"').read()
+#        n_t_c_m = sinfo.split("\n")
+#        avail_nodes = []
+#        for lines in n_t_c_m:
+#            line = lines.split()
+#            if len(line)==4 and line[1] == "idle":
+#                avail_nodes.append((line[0],int(line[2]),int(line[3])))
+#        return avail_nodes
+#
+#
+#    def get_avail_nodes_of_(partition):
+#        sinfo = os.popen('sinfo --format="%n %t %P"').read()
+#        n_t_P = sinfo.split("\n")
+#        avail_nodes = []
+#        for lines in n_t_P:
+#            line = lines.split()
+#            if len(line)==3 and line[1] == "idle" and line[2].startswith(partition):
+#                avail_nodes.append(line[0])
+#        return avail_nodes
+
+    def get_partition_info(partition="normal"):
+        sinfo = os.popen('sinfo --format="%P %c %m"').read()
+        P_c_m = sinfo.split("\n")
+        for lines in P_c_m:
+            line = lines.split()
+            if len(line)==3 and line[0].startswith(partition):
+                CPUS = int(line[1])
+                MEM  = int(line[2])
+        return CPUS, MEM
+
+    def write_submission_script(path=os.getcwd(), TURBODIR="", ntasks=8, partition="normal"):
+        s_script_path = os.path.join(path, "submit.sh")
+        s_script = open(s_script_path,"a")
+        s_script.write("#!/bin/bash\n")
+        s_script.write("#SBATCH --ntasks=%d\n" %ntasks)
+        s_script.write("#SBATCH --nodes=1\n")
+        s_script.write("#SBATCH --error=job.%J.err\n")
+        s_script.write("#SBATCH --output=job.%J.out\n")
+        s_script.write("#SBATCH --time=999:00:00\n")
+        s_script.write("#SBATCH --partition=%s\n" %partition)
+        s_script.write("#=====================================\n")
+        s_script.write("# Setup the environment for Turbomole \n")
+        s_script.write("#=====================================\n")
+        s_script.write("export PARA_ARCH=SMP\n")
+        s_script.write("export OMP_NUM_THREADS=%d\n" %ntasks)
+        s_script.write("export PARNODES=%d\n" %ntasks)
+        s_script.write("export TURBODIR=%s\n" %TURBODIR)
+        s_script.write("export PATH=$TURBODIR/bin/`sysname`:$PATH\n")
+        s_script.write("export PATH=$TURBODIR/scripts:$PATH\n")
+        s_script.write("export TURBOTMPDIR=$TMP\n")
+        s_script.write("#=====================================\n")
+        s_script.write("#  Copy every file and run the job    \n")
+        s_script.write("#=====================================\n")
+        s_script.write("sbcast submit.py $TMPDIR/submit.py\n")
+        s_script.write("cd $TMPDIR\n")
+        s_script.write("python3 submit.py > submit.out\n")
+        s_script.write("#=====================================\n")
+        s_script.write("# Copy everything back                \n")
+        s_script.write("#=====================================\n")
+        s_script.write("cp $TMPDIR/* $SLURM_SUBMIT_DIR/\n")
+        s_script.write("exit\n")
+        s_script.close()
+        return
+
+
 
 class Harvest:
     
@@ -576,10 +675,11 @@ class Harvest:
     def get_energy(self, f_ZPE = 1.0):
         aoforce_path = os.path.join(self.path, "aoforce.out")
         with open(aoforce_path) as aoforce:
-            if '  zero point VIBRATIONAL energy  ' in line:
-                self.ZPE = float(line.split()[6]) # The zero point vibrational energy in Hartree
-            if 'SCF-energy' in line:
-                self.SPE = float(line.split()[3]) # Energy in Hartree
+            for line in aoforce:
+                if '  zero point VIBRATIONAL energy  ' in line:
+                    self.ZPE = float(line.split()[6]) # The zero point vibrational energy in Hartree
+                if 'SCF-energy' in line:
+                    self.SPE = float(line.split()[3]) # Energy in Hartree
         return (self.SPE + f_ZPE*self.ZPE)
 
 
