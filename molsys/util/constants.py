5289d635ae1608b2ba4dfbb1aae311a8d9922012
Gunnar Schmitz <gunnar.schmitz@rub.de>
2021-08-03 10:07:35 +0200

    Added new constants module to replace units.py

---
+++ molsys/util/constants.py
@@ -0,0 +1,25 @@
+
+
+#
+# Molecular units
+#
+mol = 6.0221415e23 
+
+
+#
+# Length units
+#
+bohr = 0.529177249 
+angstrom = 1.889726133921252 
+ 
+#
+# Energy units
+#
+
+# base (joule)
+joule = 1/4.35974381e-18
+
+# derived from other constants
+calorie = 4.184*joule
+kcalmol = 1.0e3*calorie/mol
+electronvolt = (1.0/coulomb)*joule
